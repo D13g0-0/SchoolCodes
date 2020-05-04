53e06baf7d209a39b1904fd54be30d4242560250
D1&g0 <60611902+D13g0-0@users.noreply.github.com>
2020-05-04 09:08:18 -0300

    Add files via upload

---
+++ Double_e_Float/src/main/java/com/mycompany/double_e_float/DoubleFloat.java
@@ -0,0 +1,12 @@
+package com.mycompany.double_e_float;
+public class DoubleFloat {
+    
+    public static void main(String[] args){
+        
+        String A = "A diferênça do Float para o Double é a quantidade de números que cada um pode armazernar, Double:14 ou 15 / Float:6 ou 7";
+        System.out.println(A);
+        
+        double num = 1.000000000000001;
+        System.out.println(num); 
+    }
+}
